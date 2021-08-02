13527fb2bd52d702a582c5bdb2235432c81ea33f
Valdez Solis <aldo.valdez.solis@accenture.com>
2021-08-02 12:11:05 -0500

    Ejercicios propuestos por el instructor César para flujos de control

---
+++ ejerciciosAcademia/src/MultiplicationTable.java
@@ -0,0 +1,11 @@
+public class MultiplicationTable {
+
+    public static void main(String[] args) {
+        for (int i=1; i<11;i++){
+            System.out.println((i*1)+"\t"+(i*2)+"\t"+(i*3)+"\t"+
+                    (i*4)+"\t"+(i*5)+"\t"+(i*6)+"\t"+(i*7)+"\t"+
+                    (i*8)+"\t"+(i*9)+"\t"+(i*10));
+        }
+    }
+
+}
